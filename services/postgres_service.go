0b424184037a761f2a5eb331ad62d1757d6e9a61
mproyyan <mproyyan@gmail.com>
2023-02-03 18:55:31 +0700

    create postgres service

---
+++ services/postgres_service.go
@@ -0,0 +1,101 @@
+package services
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"sqlc-rest-api/db/postgres/repositories"
+	"sqlc-rest-api/requests"
+	"sqlc-rest-api/responses"
+)
+
+type PostgresService struct {
+	Repo repositories.Querier
+	DB   *sql.DB
+}
+
+func NewPostgresService(db *sql.DB) *PostgresService {
+	return &PostgresService{
+		Repo: repositories.New(),
+		DB:   db,
+	}
+}
+
+func (pq *PostgresService) CreateProduct(ctx context.Context, req requests.CreateProductRequest) (responses.ProductResponse, error) {
+	prod, err := pq.Repo.CreateProduct(ctx, pq.DB, req.Name)
+	if err != nil {
+		return responses.ProductResponse{}, err
+	}
+
+	return responses.ProductResponse{
+		ID:   prod.ID,
+		Name: prod.Name,
+	}, nil
+}
+
+func (pq *PostgresService) DeleteProduct(ctx context.Context, req requests.BindUriID) error {
+	prod, err := pq.Repo.GetProduct(ctx, pq.DB, req.ID)
+	if err != nil {
+		return fmt.Errorf("product with id %d not found", req.ID)
+	}
+
+	return pq.Repo.DeleteProduct(ctx, pq.DB, prod.ID)
+}
+
+func (pq *PostgresService) GetProduct(ctx context.Context, req requests.BindUriID) (responses.ProductResponse, error) {
+	prod, err := pq.Repo.GetProduct(ctx, pq.DB, req.ID)
+	if err != nil {
+		return responses.ProductResponse{}, fmt.Errorf("product with id %d not found", req.ID)
+	}
+
+	return responses.ProductResponse{
+		ID:   prod.ID,
+		Name: prod.Name,
+	}, nil
+}
+
+func (pq *PostgresService) ListProducts(ctx context.Context, req requests.ListProductsRequest) ([]responses.ProductResponse, error) {
+	arg := repositories.ListProductsParams{
+		Offset: req.Offset,
+		Limit:  req.Limit,
+	}
+
+	prods, err := pq.Repo.ListProducts(ctx, pq.DB, arg)
+	if err != nil {
+		return nil, err
+	}
+
+	var products []responses.ProductResponse
+	for _, prod := range prods {
+		p := responses.ProductResponse{
+			ID:   prod.ID,
+			Name: prod.Name,
+		}
+
+		products = append(products, p)
+	}
+
+	return products, nil
+}
+
+func (pq *PostgresService) UpdateProduct(ctx context.Context, req requests.UpdateProductRequest) (responses.ProductResponse, error) {
+	prod, err := pq.Repo.GetProduct(ctx, pq.DB, req.ID)
+	if err != nil {
+		return responses.ProductResponse{}, fmt.Errorf("product with id %d not found", req.ID)
+	}
+
+	arg := repositories.UpdateProductParams{
+		ProductID:      prod.ID,
+		NewProductName: req.Name,
+	}
+
+	updated, err := pq.Repo.UpdateProduct(ctx, pq.DB, arg)
+	if err != nil {
+		return responses.ProductResponse{}, err
+	}
+
+	return responses.ProductResponse{
+		ID:   updated.ID,
+		Name: updated.Name,
+	}, nil
+}
